1560440122551c5b5f51754406897520e92caef7
严嘉哲 <2955975006@qq.com>
2024-06-18 10:20:22 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,119 @@
+from flask import Flask, request
+from flask.views import MethodView
+from nltk import book
+
+from extension import *
+from models import Book, BookInfo
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+cors.init_app(app)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.cli.command()
+def create():
+    db.drop_all()
+    db.create_all()
+    Book.init_db()
+
+
+class BookApi(MethodView):
+    def get(self, book_id):
+        if not book_id:
+            books: [Book] = Book.query.all()
+            results = [
+                {
+                    'id': book.id,
+                    'isbn': book.isbn,
+                    'book_address': book.book_address,
+                    'book_title': book.info.book_title,
+                    'book_author': book.info.book_author,
+                    'book_publisher': book.info.book_publisher,
+                    'book_type': book.info.book_type
+                } for book in books
+            ]
+            return {
+                'status': 'success',
+                'message': '数据查询成功',
+                'results': results
+            }
+        book: Book = Book.query.get(book_id)
+        return {
+            'status': 'success',
+            'message': '数据查询成功',
+            'result': {
+                'id': book.id,
+                'isbn': book.isbn,
+                'book_address': book.book_address,
+                'book_title': book.info.book_title,
+                'book_author': book.info.book_author,
+                'book_publisher': book.info.book_publisher,
+                'book_type': book.info.book_type
+            }
+        }
+
+    def post(self):
+        form = request.json
+        book = Book()
+        book.id = form.get('id')
+        book.isbn = form.get('isbn')
+        book.book_address = form.get('book_address')
+        book_info = BookInfo()
+        book_info.isbn = form.get('isbn')
+        book_info.book_title = form.get('book_title')
+        book_info.book_author = form.get('book_author')
+        book_info.book_publisher = form.get('book_publisher')
+        book_info.book_type = form.get('book_type')
+        book.info = book_info
+        db.session.add(book)
+        db.session.commit()
+
+        return {
+            'status': 'success',
+            'message': '数据添加成功'
+        }
+
+    def delete(self, book_id):
+        book = Book.query.get(book_id)
+        db.session.delete(book)
+        db.session.commit()
+
+        return {
+            'status': 'success',
+            'message': '数据删除成功'
+        }
+
+    def put(self, book_id):
+        form = request.json
+        book = Book.query.get(book_id)
+        book.isbn = form.get('isbn')
+        book.book_address = form.get('book_address')
+        book_info = BookInfo.query.get(book.isbn)
+        book_info.book_title = form.get('book_title')
+        book_info.book_author = form.get('book_author')
+        book_info.book_publisher = form.get('book_publisher')
+        book_info.book_type = form.get('book_type')
+        db.session.commit()
+
+        return {
+            'status': 'success',
+            'message': '数据更新成功'
+        }
+
+
+
+book_view = BookApi.as_view('book_api')
+app.add_url_rule('/book/', defaults={'book_id': None}, view_func=book_view, methods=['GET'])
+app.add_url_rule('/book/', view_func=book_view, methods=['POST'])
+app.add_url_rule('/book/<int:book_id>', view_func=book_view, methods=['GET', 'PUT', 'DELETE'])
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', 5000)
---
+++ models.py
@@ -0,0 +1,38 @@
+from extension import db
+
+
+class BookInfo(db.Model):
+    __tablename__ = 'book_info'
+    isbn = db.Column(db.Integer, primary_key=True)
+    book_title = db.Column(db.String(255), unique=True, nullable=False)
+    book_author = db.Column(db.String(255), unique=True, nullable=False)
+    book_publisher = db.Column(db.String(255), unique=True, nullable=False)
+    book_type = db.Column(db.String(255), unique=True, nullable=False)
+
+
+class Book(db.Model):
+    __tablename__ = 'books'
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.ForeignKey('book_info.isbn'), nullable=False)
+    book_address = db.Column(db.String(255), unique=True, nullable=False)
+    info = db.relationship('BookInfo', backref='info', lazy=True)
+
+    @staticmethod
+    def init_db():
+        rets = [
+            (1, 9787506365437, '书架1', '活着', '余华', '北京作家出版社', '小说'),
+            (2, 9787536692930, '书架2', '三体', '刘慈欣', '重庆出版社', '科幻')
+        ]
+        for ret in rets:
+            book = Book()
+            book_info = BookInfo()
+            book.id = ret[0]
+            book_info.isbn = ret[1]
+            book.book_address = ret[2]
+            book_info.book_title = ret[3]
+            book_info.book_author = ret[4]
+            book_info.book_publisher = ret[5]
+            book_info.book_type = ret[6]
+            book.info = book_info
+            db.session.add(book)
+        db.session.commit()
