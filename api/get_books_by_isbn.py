420909b3589ae4c51c3bf66596291763fc4a20ba
严嘉哲 <2955975006@qq.com>
2024-06-18 17:54:45 +0800

    Update get_books_by_isbn.py

--- api/get_books_by_isbn.py
+++ api/get_books_by_isbn.py
@@ -55,13 +55,40 @@ class BooksApi(MethodView):
         if book is None:
             return jsonify({'error': 'Book not found'}), 404
 
+        data = request.get_json()
+        book_info = BookInfo.query.filter_by(isbn=isbn).first()
+        book.book_address = data.get('book_address')
+        if book_info is None:
+            book_info = BookInfo()
+            book_info.isbn = isbn
+            book_info.book_title = data.get('book_title')
+            book_info.book_author = data.get('book_author')
+            book_info.book_publisher = data.get('book_publisher')
+            book_info.book_type = data.get('book_type')
+            db.session.add(book_info)
+        book.info = book_info
+        db.session.add(book)
+        db.session.commit()
         db.session.delete(book)
         db.session.commit()
 
         return jsonify({'status': 'success', 'message': '数据删除成功'}), 200
 
 
+    def put(self, isbn):
+        data = request.get_json()
+        book = Book.query.filter_by(isbn=isbn).first()
+        if book is None:
+            return jsonify({'error': 'Book not found'}), 404
+
+        book.book_address = data.get('book_address')
+
+        db.session.commit()
+
+        return jsonify({'status': 'success', 'message': '数据修改成功'}), 200
+
+
 books_api = blueprints.Blueprint('books_api', __name__)
 book_view = BooksApi.as_view('books_api')
 books_api.add_url_rule('/', view_func=book_view, methods=['POST'])
-books_api.add_url_rule('/<int:isbn>', view_func=book_view, methods=['GET', 'DELETE'])
+books_api.add_url_rule('/<int:isbn>', view_func=book_view, methods=['GET', 'DELETE', 'PUT'])
